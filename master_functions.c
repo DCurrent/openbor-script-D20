737d808c38907fa1a447eb76108de929373d6e51
DCurrent <dc@caskeys.com>
2026-05-28 23:03:32 -0400

    Fix bone-headed folder shuffle in previous commit.

--- master_functions.c
+++ master_functions.c
@@ -1,5 +1,20 @@
-#include "data/scripts/dc_d20/master_config.h"
+/*
+ * DC D20 Library
+ * Copyright © Damon V. Caskey. All rights reserved.
+ *
+ * Royalty-free use is permitted for OpenBOR modules and related projects.
+ * Original author credit, copyright notices, and license notices must remain
+ * intact in the source code. Projects using this library must credit
+ * Damon V. Caskey for the DC D20 library in a visible module credit,
+ * readme, documentation, website, or equivalent location.
+ *
+ * This software is provided "as is", without warranty of any kind.
+ * Damon V. Caskey assumes no liability for damages, losses, or other claims
+ * arising from use, misuse, modification, distribution, or inability to use
+ * this software.
+ */
 
+#include "data/scripts/dc_d20/master_config.h"
 
 /* 
 * Caskey, Damon V.
